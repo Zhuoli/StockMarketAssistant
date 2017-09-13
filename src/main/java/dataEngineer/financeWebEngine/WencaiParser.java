e5369c32dabb10e612ac662fff8df761000d6942
Zhuoli Liang <zhuoliseattle@gmail.com>
2017-09-12 18:23:46 -0700

    Catch No More exception

--- src/main/java/dataEngineer/financeWebEngine/WencaiParser.java
+++ src/main/java/dataEngineer/financeWebEngine/WencaiParser.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 /**
@@ -16,15 +17,22 @@ import java.util.stream.Collectors;
 public class WencaiParser {
     static String StockSearch = "http://www.iwencai.com/stockpick/search?typed=1&preParams=&ts=1&f=1&qs=index_rewrite&selfsectsn=&querytype=&searchfilter=&tid=stockpick&w=";
 
-
+    {
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+    }
     public static List<String> retrieveConcepts(String symbol) {
+        WebDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME, true);
         try {
             if (symbol.toLowerCase().contains("sz") || symbol.toLowerCase().contains("sh"))
                 symbol = symbol.substring(2);
-            WebDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME, true);
             driver.get(WencaiParser.StockSearch + symbol);
-            WebElement moreElement = driver.findElement(By.cssSelector("div[class='em alignCenter split'] a[class='ml5 moreSplit fr']"));
-            moreElement.click();
+            try {
+                WebElement moreElement = driver.findElement(By.cssSelector("div[class='em alignCenter split'] a[class='ml5 moreSplit fr']"));
+                moreElement.click();
+            }catch (org.openqa.selenium.NoSuchElementException noexc){
+                // Ignore
+            }
             List<WebElement> conceptElements = driver.findElements(By.cssSelector("div[class='em alignCenter split'] span a"));
             return conceptElements.stream().map(element -> element.getText()).collect(Collectors.toList());
         }catch (Exception exc){
